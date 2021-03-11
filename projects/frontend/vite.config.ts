056ed20ac656cd4d214b56b2b26339c4947f8529
崮生 <admin@shenzilong.cn>
2021-03-11 20:17:08 +0800

    🐳chore(package.json): 配置编译脚本

--- projects/frontend/vite.config.ts
+++ projects/frontend/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build:{
-    outDir:'../../.malagu/frontend/dist/'
-  }
-})
+  build: {
+    outDir: "../../.malagu/frontend/dist/",
+    emptyOutDir: true,
+  },
+});
