71fbf9dd484eca1f267163c94bd7f8b805b3afb0
崮生 <admin@shenzilong.cn>
2021-03-12 19:08:10 +0800

    ✨feat(frontend): 引入 tailwind

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -4,27 +4,21 @@
       >serverless版</a
     >)
   </h1>
-  <label style="display: flex; width: 100%">
-    <span style="width: 30%; flex-shrink: 0">使用该字体的文字：</span>
-    <input type="text" v-model="text" style="flex: 1" />
+  <label class="flex w-full">
+    <span class="w-1/3 flex-shrink-0">使用该字体的文字：</span>
+    <input type="text" v-model="text" class="flex-1" />
   </label>
-  <label style="display: flex; width: 100%">
-    <span style="width: 30%; flex-shrink: 0">选择使用的字体：</span>
-    <input disabled type="text" v-model="font" style="flex: 1" />
+  <label class="flex w-full">
+    <span class="w-1/3 flex-shrink-0">选择使用的字体：</span>
+    <input disabled type="text" v-model="font" class="flex-1" />
   </label>
-  <label style="display: flex; width: 100%">
-    <span style="width: 30%; flex-shrink: 0">选择字体类型：</span>
-    <input disabled type="text" v-model="type" style="flex: 1" />
+  <label class="flex w-full">
+    <span class="w-1/3 flex-shrink-0">选择字体类型：</span>
+    <input disabled type="text" v-model="type" class="flex-1" />
   </label>
 
   <span>你可以直接 copy 下面的 css 去使用</span>
-  <textarea
-    v-model="cssCode"
-    disabled
-    :cols="20"
-    :rows="16"
-    style="width: 95%; font-family: test"
-  />
+  <textarea v-model="cssCode" disabled :cols="20" :rows="16" class="w-11/12" />
 
   <footer>
     <a href="https://github.com/2234839/web-font/tree/serverless"
@@ -60,8 +54,6 @@
       return { cssCode, style, text, font, temp, type };
     },
   });
-
-  const defaultCssCode = ``;
 </script>
 
 <style>
